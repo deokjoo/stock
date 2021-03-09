820bf3be134dc1226a7f549743232e37a4c93027
deokjoo <oriho@daum.net>
2021-03-09 23:37:15 +0900

    code cleaning..

--- s00.py
+++ s00.py
@@ -2,47 +2,55 @@
 import re
 from requests import Request, Session
 from fake_useragent import UserAgent
-from urllib import parse
+from urllib.parse import unquote
+import os
 
 from io import BytesIO
 import zipfile
 
+import xmltodict
 
+'''
+my API_KEY!!
+'''
 apiKey = "f1f64632cabe19da450a98456722f7bccf8d5c0f"
 
-# https://opendart.fss.or.kr/api/corpCode.xml
+'''
+download xmlFile from Dart
+'''
+def downloadCorpCode(FileUrl):
 
-def download(FileUrl):
-    from .spinner import Spinner
-    from .file import create_folder
-    from urllib.parse import unquote
-    import os
+    with Session() as s:
+        s.headers.update({'user-agent': UserAgent().chrome})
 
+        req = Request('GET', FileUrl, params={'crtfc_key':apiKey})
+        prepped = s.prepare_request(req)
+        resp = s.send(prepped)
 
-    s = Session()
-    s.headers.update({'user-agent': UserAgent().chrome})
-
-
-    data = {'crtfc_key':apiKey}
-
-    req = Request('GET', FileUrl, params=data)
-    prepped = s.prepare_request(req)
-
-    resp = s.send(prepped)
+    # check header
     headers = resp.headers.get('Content-Disposition')
     if headers is None or not re.search('attachment', headers):
-        raise FileNotFoundError('target does not exist')
+        return []
 
-    filename = unquote(re.findall(r'filename="?([^"]*)"?', headers)[0])
-    with open(filename, "wb") as f:
-        f.write(BytesIO(resp.content).getbuffer())
+    z = zipfile.ZipFile(BytesIO(resp.content))
+    z.extractall("./data/")
+    z.close()
 
-    return filename
+    return z.namelist()
 
-
-
-
-if __name__ == "__main__":
+def getCorpCodeFromDart():
     url = "https://opendart.fss.or.kr/api/corpCode.xml"
+    xmlFile = downloadCorpCode(url)
 
-    download(url)
+    return xmlFile
+
+'''
+'''
+def makeDarkDB():
+    with open("./data/CORPCODE.xml") as fd:
+        doc = xmltodict.parse(fd.read())
+
+'''
+'''
+if __name__ == "__main__":
+    getCorpCodeFromDart()
