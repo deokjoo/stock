9b83c6ab3cbf2acd7e73b23fe60b2893f5ab2220
deokjoo <oriho@daum.net>
2021-03-10 10:44:11 +0900

    make dataframe

--- s00.py
+++ s00.py
@@ -9,6 +9,8 @@ from io import BytesIO
 import zipfile
 
 import xmltodict
+import collections
+import pandas as pd
 
 '''
 my API_KEY!!
@@ -33,7 +35,7 @@ def downloadCorpCode(FileUrl):
         return []
 
     z = zipfile.ZipFile(BytesIO(resp.content))
-    z.extractall("./data/")
+    z.extractall()
     z.close()
 
     return z.namelist()
@@ -42,15 +44,53 @@ def getCorpCodeFromDart():
     url = "https://opendart.fss.or.kr/api/corpCode.xml"
     xmlFile = downloadCorpCode(url)
 
-    return xmlFile
+    return xmlFile[0]
 
 '''
 '''
-def makeDarkDB():
-    with open("./data/CORPCODE.xml") as fd:
-        doc = xmltodict.parse(fd.read())
+
+
+'''
+'''
+def darkListFormatting(doc):
+    # mangling..
+    dartSrc = collections.defaultdict(list)
+
+    for item in doc['result']['list']:
+        for key in item:
+            dartSrc[key].append(item[key])
+
+    # merge by stock coce
+    df1 = pd.DataFrame  (dartSrc)
+    df2 = pd.read_excel ("./data/list.xlsx")
+    df2['종목코드'] = df2['종목코드'].apply(lambda x: '{:0>6d}'.format(x))
+
+    df3 = pd.merge(df2, df1, how="left", left_on="종목코드", right_on="stock_code")
+    df3 = df3[['회사명', '종목코드', 'corp_code','업종', '주요제품', '상장일', '결산월', '대표자명', '홈페이지', '지역']]
+    df3.to_excel("./data/ref.xlsx")
+
+    return df3
+
+'''
+TODO: use halo lib for texting
+'''
+def makeDarkDB(loadFromDart=False):
+    dartFile = "CORPCODE.xml"
+
+    if loadFromDart == True:
+        print("download file form DART")            
+        dartFile = getCorpCodeFromDart()
+    try:
+        with open(dartFile) as fd:
+            doc = xmltodict.parse(fd.read())
+    except Exception() as e:
+        return None
+
+    df = darkListFormatting(doc)
+
+    return df
 
 '''
 '''
 if __name__ == "__main__":
-    getCorpCodeFromDart()
+    df = makeDarkDB(loadFromDart=True)
