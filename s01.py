6c89d81da4a38ccb1fbbd28f5ca7ea367e66b7d2
deokjoo <oriho@daum.net>
2021-03-10 18:48:44 +0900

    ing.

--- s01.py
+++ s01.py
@@ -11,6 +11,7 @@ import xmltodict
 import collections
 import pandas as pd
 import json
+import os.path
 
 apiKey       = "f1f64632cabe19da450a98456722f7bccf8d5c0f"
 dartSingleAnt= "https://opendart.fss.or.kr/api/fnlttSinglAcnt.json"
@@ -37,13 +38,23 @@ def getSingleAccount(corp_code, ts, te):
     #
     accDic   = {}
     for year in range(ts, te):
-        param0 = \
-        {
-            "crtfc_key" : apiKey, "corp_code" : corp_code, "bsns_year" : str(year), "reprt_code":"11011"
-        }
 
-        resp = getFromDart(dartSingleAnt, param0) 
-        data = json.loads(resp.content)
+        fname = "./data/" + corp_code+ "_" + str(year)+".json"
+
+        if os.path.isfile(fname) == True:
+            with open(fname, "r") as json_file:
+                data = json.load(json_file)
+        else:
+            param0 = {
+                "crtfc_key" : apiKey, "corp_code" : corp_code, "bsns_year" : str(year), "reprt_code":"11011"
+            }
+            resp = getFromDart(dartSingleAnt, param0)
+
+            '''parsing'''
+            data = json.loads(resp.content)
+            with open(fname, "w") as json_file:
+                json.dump(data, json_file)
+
         if data['status'] != '000':
             continue
 
@@ -66,7 +77,6 @@ def getAccount(*argv):
     accDic = getSingleAccount(*argv)
 
     df = pd.DataFrame(accDic).T
-
     df = df.apply( lambda x : x.str.replace(",",""))
     # df = df.astype("float")
 
@@ -76,7 +86,7 @@ def getAccount(*argv):
 '''
 '''
 if __name__ =="__main__":
-    # company = ['00126380', '00401731']
+    # part = ['00126380', '00401731']
 
     # with pd.ExcelWriter("1.xlsx") as writer:
     #     for i, com in  enumerate(company):
@@ -87,7 +97,7 @@ if __name__ =="__main__":
     part    = list(company['corp_code'][:-1])
 
     with pd.ExcelWriter("1.xlsx") as writer:
-        for com in part:
-            print(com)
+        for i,com in enumerate(part):
+            print(i, com)
             a = getAccount(com, 2015, 2021)
-            a.to_excel(writer, com)
+            # a.to_excel(writer, com)
